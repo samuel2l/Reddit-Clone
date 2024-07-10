b7503a6de91c53f65ccb11cf7c0c1739e301482a
Samuel Adams <sama29571@gmail.com>
2024-07-10 12:20:32 +0000

    x

--- lib/features/community/screens/community_screen.dart
+++ lib/features/community/screens/community_screen.dart
@@ -22,7 +22,46 @@ class CommunityScreen extends ConsumerWidget {
                   Positioned.fill(child: Image.network(data.banner,fit: BoxFit.cover,),),
                 ],
               ),
-            )
+            ),
+            SliverPadding(
+                    padding: const EdgeInsets.all(16),
+                    sliver: SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: CircleAvatar(
+                              backgroundImage: NetworkImage(data.dp),
+                              radius: 35,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'r/${data.name}',
+                                style: const TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+OutlinedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(20),
+                                          ),
+                                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                                        ),
+                                        child: Text('Join'),
+                                      ),
+                          ],
+                          ),
+                          
+                          ],
+                          ),
+                          ),)
 
 
           ];
