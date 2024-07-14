dd3431765a52c93bd0303e3fd16e937f756b4c34
Samuel Adams <sama29571@gmail.com>
2024-07-14 13:49:01 +0000

    X

--- lib/features/community/screens/edit_community.dart
+++ lib/features/community/screens/edit_community.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit/constants/constants.dart';
 import 'package:reddit/features/community/controller/community_controller.dart';
 import 'package:reddit/themes/pallette.dart';
+import 'package:reddit/utils.dart';
 
 class EditCommunity extends ConsumerStatefulWidget {
   final String name;
@@ -14,6 +18,27 @@ class EditCommunity extends ConsumerStatefulWidget {
 }
 
 class _EditCommunityState extends ConsumerState<EditCommunity> {
+File? bannerImg;
+File? dp;
+
+void pickBanner()async{
+  final res= await pickImage();
+  if(res!=null){
+    setState(() {
+      bannerImg=File(res.files.first.path!);
+    });
+  }
+
+}
+void pickDp()async{
+  final res= await pickImage();
+  if(res!=null){
+    setState(() {
+      dp=File(res.files.first.path!);
+    });
+  }
+
+}
 
   @override
   Widget build(BuildContext context) {
@@ -37,29 +62,41 @@ class _EditCommunityState extends ConsumerState<EditCommunity> {
             children: [
               Column(
                 children: [
-                  DottedBorder(borderType: BorderType.RRect,radius: const Radius.circular(10),dashPattern:const [10,4],
-                  strokeCap: StrokeCap.round,
-                  color: Colors.white,
-              
-                  child: Container(
-                    
-                    height: 150,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(7)
+                  GestureDetector(
+                    onTap: pickBanner,
+                    child: DottedBorder(borderType: BorderType.RRect,radius: const Radius.circular(10),dashPattern:const [10,4],
+                    strokeCap: StrokeCap.round,
+                    color: Colors.white,
+                                  
+                    child: Container(
+                      
+                      height: 150,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(7)
+                      ),
+                      child:bannerImg!=null?Image.file(bannerImg!,fit:BoxFit.cover,): data.banner.isEmpty || data.banner==bannerDefault? const Center(child: Icon(Icons.camera_alt_outlined,size: 40,),) : Image.network(data.banner),
                     ),
-                    child: data.banner.isEmpty || data.banner==bannerDefault? const Center(child: Icon(Icons.camera_alt_outlined,size: 40,),) : Image.network(data.banner),
-                  ),
+                    ),
                   ),
                 ],
               ),
-              Positioned(
-                left: 20,
-                bottom: 20,
-                child: CircleAvatar(backgroundImage: NetworkImage(data.dp),
-                          radius: 42,
-                ),
-              )
+Positioned(
+  left: 20,
+  bottom: 20,
+  child: GestureDetector(
+    onTap: pickDp,
+    child: dp!=null?CircleAvatar(
+      backgroundImage: FileImage(dp!),
+
+radius: 32,
+    ):CircleAvatar(
+      backgroundImage: NetworkImage(data.dp),
+
+radius: 32,
+    )
+  ),
+)
             ],
           ),
         ),
