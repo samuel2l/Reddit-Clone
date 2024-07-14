0cce863a902bb46a373772db3d75c4318472e725
Samuel Adams <sama29571@gmail.com>
2024-07-14 14:36:53 +0000

    x

---
+++ lib/providers/storage_repository_providers.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:reddit/failure.dart';
+import 'package:reddit/type_defs.dart';
+
+class StorageRepository{
+  final FirebaseStorage _firebaseStorage;
+
+  StorageRepository({required FirebaseStorage firebaseStorage}) : _firebaseStorage = firebaseStorage;
+
+FutureEither<String> storeFile({required String path,required String id, required File? file})async{
+ try{
+  final ref= _firebaseStorage.ref().child(path).child(id);
+  UploadTask uploadTask=ref.putFile(file!);
+  final snapshot=await uploadTask;
+  return right(await snapshot.ref.getDownloadURL());
+// ```dart
+// - **Create a Storage Reference**: 
+//   ```dart
+//   final ref = _firebaseStorage.ref().child(path).child(id);
+//   ```
+//   - This creates a reference to the location in Firebase Storage where the file will be stored.
+//   - `_firebaseStorage.ref()`: The root reference of Firebase Storage.
+//   - `.child(path)`: A child reference under the root, specified by the `path` parameter.
+//   - `.child(id)`: A child reference under the path, specified by the `id` parameter.
+
+//   - `ref.putFile(file!)`: Starts uploading the file to the specified reference
+//   - `UploadTask`: Represents the task of uploading the file.
+
+//   final snapshot = await uploadTask;
+//   - This line waits for the upload task to complete and obtains the snapshot of the upload.
+
+// - **Return the Download URL**:
+//   return right(await snapshot.ref.getDownloadURL());
+//   - `snapshot.ref.getDownloadURL()`: Gets the download URL of the uploaded file.
+//   - `right(await snapshot.ref.getDownloadURL())`: Wraps the download URL in a `Right` to indicate a successful operation.
+
+ }catch(e){
+  return left(Failure(e.toString()));
+ } 
+}
+}
--- lib/type_defs.dart
+++ lib/type_defs.dart
@@ -2,4 +2,9 @@ import 'package:fpdart/fpdart.dart';
 import 'package:reddit/failure.dart';
 
 typedef FutureEither<T> = Future<Either<Failure, T>>;
+// Future<Either<Failure, T>> means that you have an asynchronous operation (Future) that will eventually complete with an Either value. The Either can be:
+
+// Left<Failure>: Represents a failure, containing a Failure object.
+// Right<T>: Represents a success, containing a value of type T.
+
 typedef FutureVoid = FutureEither<void>;
