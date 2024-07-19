3ab8a902e87d6b66c013357f9f5299cf65e673fd
Samuel Adams <sama29571@gmail.com>
2024-07-19 06:49:46 +0000

    x

---
+++ lib/features/home/delegates/search_community_delegates.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SearchCommunityDelegate extends SearchDelegate{
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+   return [
+    IconButton(onPressed: (){
+      query='';
+    }, icon: const Icon(Icons.close))
+
+   ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+ return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+return const SizedBox();  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    throw UnimplementedError();
+  }
+
+
+}
