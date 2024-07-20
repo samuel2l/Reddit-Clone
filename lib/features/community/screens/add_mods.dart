3ebce7513e6119f9a969d4fdea682a552403e3bf
Samuel Adams <sama29571@gmail.com>
2024-07-20 16:22:30 +0000

    added add mods screen

--- lib/features/community/screens/add_mods.dart
+++ lib/features/community/screens/add_mods.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit/features/auth/controller/auth_controller.dart';
 import 'package:reddit/features/community/controller/community_controller.dart';
 
 class AddMods extends ConsumerStatefulWidget {
@@ -12,8 +13,22 @@ final String name;
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _AddModsState();
 }
-
 class _AddModsState extends ConsumerState<AddMods> {
+  Set<String> uIds={};
+  //add a counter so you can select and deselect
+  //logic is so when build func re runs when you call set state the users id is not added to uids set unless the count is at 0
+  int count=0;
+  void addUid(String uId){
+    setState(() {
+      uIds.add(uId);
+    });
+  }
+  void removUid(String uId){
+    setState(() {
+      uIds.remove(uId);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -21,7 +36,7 @@ class _AddModsState extends ConsumerState<AddMods> {
         actions: [
           IconButton(
             onPressed: (){},
-           icon:const Icon(Icons.save)
+           icon:const Icon(Icons.done)
 ),
         ],
       ),
@@ -29,11 +44,29 @@ class _AddModsState extends ConsumerState<AddMods> {
 return ListView.builder(
   itemCount: community.members.length,
   itemBuilder: (context, index){
-    return ListTile(
-      leading: const Icon(Icons.person),
-      title: Text(community.members[index]),
+    // community.members[index] returns the users uId
+    // so to get the members name we will use the getUserData provider which takes a uId as a param
+    //since we are using another provider we will have another .when nested you barb
+    return ref.watch(getUserDataProvider(community.members[index])).when(data:(user){
+    if(community.mods.contains(community.members[index]) && count==0){
+      uIds.add(community.members[index]);
+    }
+
+  count++;
+    return CheckboxListTile(
+
+value: uIds.contains(user!.uId),
+//if the value property is true then it means the list tile will be checked
+onChanged: (value) {
+  
+},
+      title: Text(user.name),
 
     );
+
+    } ,error:(error,stackTrace)=> Text(error.toString()), loading:()=> const Center(child:CircularProgressIndicator(),
+      ), );
+
   },);
       }, error:(error,stackTrace)=> Text(error.toString()), loading:()=> const Center(child:CircularProgressIndicator(),
       ),
